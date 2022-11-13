e93adb51694ca46722e1ff9c4395f5b0f6c8b63e
Koofem <49820394+Koofem@users.noreply.github.com>
2022-11-13 15:16:44 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ sudo apt update && sudo apt -y upgrade
 sudo apt install -y curl git wget vim
 sudo apt install -y zsh
 yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sleep 1
 sudo chsh -s $(which zsh) $(whoami)
+sleep 1
 yes | curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 sudo source .zshrc
 sudo nvm install --lts
