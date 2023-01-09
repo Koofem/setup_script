a0b650f67ca5dfd986b19e4f12c1edb784ef8fbd
Koofem <49820394+Koofem@users.noreply.github.com>
2023-01-09 16:11:42 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,7 @@ sudo apt update && sudo apt -y upgrade
 sudo apt install -y curl git wget vim
 sudo apt install -y zsh
 yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sleep 1
 sudo chsh -s $(which zsh) $(whoami)
-sleep 1
 yes | curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 echo 'export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
